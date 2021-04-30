36a83c835362848483c628c83e1ccf2728f166ee
Thomas <substantialimpulse@pm.me>
2021-04-30 14:31:05 +0200

    export to json for testing in frontend

--- reims_dbf_extractor/reims_dbf_extractor.py
+++ reims_dbf_extractor/reims_dbf_extractor.py
@@ -1,7 +1,7 @@
 """Main module."""
 from pathlib import Path
 
-import dbfread
+import json
 import pandas as pd
 from dbfread import DBF
 
@@ -19,8 +19,21 @@ files = [
 for file in files:
     try:
         print("\n" + file + "\n---------------------------")
-        dbf = DBF(p / file, load=True, char_decode_errors='ignore')
+        dbf = DBF(p / file, char_decode_errors='ignore')
         frame = pd.DataFrame(iter(dbf))
         print(frame)
     except Exception as e:
         print(e)
+
+# Converting to JSON for testing in frontend
+dbf_main = DBF(p / "Glsku.dbf")
+df = pd.DataFrame(iter(dbf_main))
+# drop unused columns
+df = df.drop(columns=['TINT', 'ENTERDATE'])
+# remove RXX: prefix from SKU (not needed)
+df['SKU'] = df['SKU'].apply(lambda x: x.split(":")[-1])
+
+# convert to JSON and directly save in frontend assets foldeer
+dfj = json.loads(df.reset_index().to_json(orient='records'))
+with open(Path("../").resolve() / "reims2-frontend/assets/out.json", 'w', encoding='utf-8') as f:
+    json.dump(dfj, f, ensure_ascii=False, indent=4)
