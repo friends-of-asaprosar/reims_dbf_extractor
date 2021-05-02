f73ce446d7dd2e2cb1d3f8604cd4b7ba53dfc622
l3d00m <substantialimpulse@protonmail.com>
2021-05-02 12:53:57 +0200

    rename enum types in DB

--- reims_dbf_extractor/reims_dbf_extractor.py
+++ reims_dbf_extractor/reims_dbf_extractor.py
@@ -5,17 +5,39 @@ import json
 import pandas as pd
 from dbfread import DBF
 
+
+def map_size(x):
+    if x == 'S':
+        return 'small'
+    if x == 'C':
+        return 'child'
+    if x == 'L':
+        return 'large'
+    if x == 'M':
+        return 'medium'
+    return 'unknown'
+
+
+def map_gender(x):
+    if x == 'M':
+        return 'masculine'
+    if x == 'F':
+        return 'feminin'
+    if x == 'U':
+        return 'neutral'
+    return 'unknown'
+
+
 p = Path('files')
 files = [
     "Glsku.dbf",
-    "Dispense.dbf",
-    "Rdinv.dbf",
-    "Rdtrak.dbf",
-    "Rdadd.dbf",
-    "Readd.dbf",
-    "Deleted.dbf"
+    # "Dispense.dbf",
+    # "Rdinv.dbf",
+    # "Rdtrak.dbf",
+    # "Rdadd.dbf",
+    # "Readd.dbf",
+    # "Deleted.dbf"
 ]
-
 for file in files:
     try:
         print("\n" + file + "\n---------------------------")
@@ -28,10 +50,16 @@ for file in files:
 # Converting to JSON for testing in frontend
 dbf_main = DBF(p / "Glsku.dbf")
 df = pd.DataFrame(iter(dbf_main))
-# drop unused columns
-df = df.drop(columns=['TINT', 'ENTERDATE'])
+
 # remove RXX: prefix from SKU (not needed)
 df['SKU'] = df['SKU'].apply(lambda x: x.split(":")[-1])
+df['TYPE'] = df['TYPE'].apply(lambda x: 'single' if x == 'S' else 'bifocal' if x == 'B' else 'unknown')
+df['SIZE'] = df['SIZE'].apply(map_size)
+df['APPEARANCE'] = df['APPEARANCE'].apply(map_gender)
+df['MATERIAL'] = df['MATERIAL'].apply(lambda x: 'plastic' if x == 'P' else 'metal' if x == 'M' else 'unknown')
+
+# drop unused columns
+df = df.drop(columns=['TINT', 'ENTERDATE', 'APPEARANCE'])
 
 # convert to JSON and directly save in frontend assets foldeer
 dfj = json.loads(df.reset_index().to_json(orient='records'))
