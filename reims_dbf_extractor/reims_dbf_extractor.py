990dc27b6c84306a20a96beed7e4182e53a3a0a9
Thomas <substantialimpulse@pm.me>
2021-05-26 22:12:52 +0200

    clean up code, add CSV export function

--- reims_dbf_extractor/reims_dbf_extractor.py
+++ reims_dbf_extractor/reims_dbf_extractor.py
@@ -1,11 +1,12 @@
-"""Main module."""
+import asyncio
+import csv
+import json
 from pathlib import Path
 
-import json
+import aiohttp
+from attr.setters import convert
 import pandas as pd
 from dbfread import DBF
-import asyncio
-import aiohttp
 
 
 def map_size(x):
@@ -30,70 +31,74 @@ def map_gender(x):
     return 'unknown'
 
 
-p = Path('files')
-files = [
-    "Glsku.dbf",
-    # "Dispense.dbf",
-    # "Rdinv.dbf",
-    # "Rdtrak.dbf",
-    # "Rdadd.dbf",
-    # "Readd.dbf",
-    # "Deleted.dbf"
-]
-for file in files:
-    try:
-        print("\n" + file + "\n---------------------------")
-        dbf = DBF(p / file, char_decode_errors='ignore')
-        frame = pd.DataFrame(iter(dbf))
-        print(frame)
-    except Exception as e:
-        print(e)
+def convert_to_dict(df):
+    # remove RXX: prefix from SKU (not needed)
+    df['SKU'] = df['SKU'].apply(lambda x: x.split(":")[-1].split(" ")[0])
+    df['TYPE'] = df['TYPE'].apply(lambda x: 'single' if x == 'S' else 'bifocal' if x == 'B' else 'unknown')
+    df['SIZE'] = df['SIZE'].apply(map_size)
+    df['APPEARANCE'] = df['GENDER'].apply(map_gender)
+    df = df.drop(columns=['TINT', 'GENDER', 'MATERIAL'])
+    df.columns = map(str.lower, df.columns)
+
+    # with open(Path("/home/thomas/Documents/wichtig/reims/new-data/csv/sm_" + file.split(".")[0] + ".csv"), 'w', encoding='utf-8') as f:
+    #     df.to_csv(f)
+
+    final = []
+    for i, col in enumerate(df.iterrows()):
+        col = col[1]
+        if col['sku'] == '':
+            continue
+        if any(c.isalpha() for c in col['sku']):
+            col['sku'] = -1
+        od_df = {'sphere': col['odsphere'], 'axis': col['odaxis'], 'cylinder': col['odcylinder']}
+        os_df = {'sphere': col['ossphere'], 'axis': col['osaxis'], 'cylinder': col['oscylinder']}
+        if col['type'] != 'single':
+            od_df['add'] = col['odadd']
+            os_df['add'] = col['osadd']
+        location = 'sm' if int(col['sku']) > 5000 else 'sa'
+        final.append({'sku': int(col['sku']), 'glassesType': col['type'], 'od': od_df, 'os': os_df,
+                      'appearance': col['appearance'], 'glassesSize': col['size'], 'dispense': {}, 'location': location})
+    return final
+
+
+# p = Path('/home/thomas/Documents/wichtig/reims/new-data/SM_2020')
+# files = [
+#     "Dispense.dbf",
+#     "Glsku.dbf",
+#     "NOTFOUND.dbf"
+# ]
+# for file in files:
+#     print("\n" + file + "\n---------------------------")
+#     dbf = DBF(p / file, char_decode_errors='ignore')
+#     frame = pd.DataFrame(iter(dbf))
+#     print(frame)
+#     dict_list = convert_to_dict(frame)
 
 # Converting to JSON for testing in frontend
-dbf_sa = DBF(p / "GLSKU_SA.dbf")
-dbf_sm = DBF(p / "GLSKU_SM.dbf")
+dbf_sa = DBF(Path("files/GLSKU_SA.dbf"))
+dbf_sm = DBF(Path("files/GLSKU_SM.dbf"))
 df_sa = pd.DataFrame(iter(dbf_sa))
 df_sm = pd.DataFrame(iter(dbf_sm))
 df = pd.concat([df_sa, df_sm])
-
-# remove RXX: prefix from SKU (not needed)
-df['SKU'] = df['SKU'].apply(lambda x: x.split(":")[-1].split(" ")[0])
-df['TYPE'] = df['TYPE'].apply(lambda x: 'single' if x == 'S' else 'bifocal' if x == 'B' else 'unknown')
-df['SIZE'] = df['SIZE'].apply(map_size)
-df['APPEARANCE'] = df['GENDER'].apply(map_gender)
-df.columns = map(str.lower, df.columns)
-
-
-final = []
-# drop unused columns
-for i, col in enumerate(df.iterrows()):
-    col = col[1]
-    od_df = {'sphere': col['odsphere'], 'axis': col['odaxis'], 'cylinder': col['odcylinder']}
-    os_df = {'sphere': col['ossphere'], 'axis': col['osaxis'], 'cylinder': col['oscylinder']}
-    if col['type'] != 'single':
-        od_df['add'] = col['odadd']
-        os_df['add'] = col['osadd']
-    location = 'sm' if int(col['sku']) > 5000 else 'sa'
-    final.append({'id': i, 'sku': col['sku'], 'glassesType': col['type'], 'od': od_df, 'os': os_df,
-                  'appearance': col['appearance'], 'glassesSize': col['size'], 'dispense': {}, 'material': 'any', 'location': location})
+final = convert_to_dict(df)
 
 
 # with open(Path("../").resolve() / "reims2-frontend/assets/out.json", 'w', encoding='utf-8') as f:
-#    json.dump(final, f, ensure_ascii=False, indent=4)
-
+#     json.dump(final, f, ensure_ascii=False, indent=4)
 
 # Upload URLs to backend
 
-async def make_parallel_post(data, i):
+
+async def make_parallel_post(data):
     async with aiohttp.ClientSession() as session:
         async with session.post("https://api.reims2.duckdns.org/pvh/api/glasses", json=data) as resp:
-            print(resp.status, i)
+            print(resp.status, data['sku'])
 
 loop = asyncio.get_event_loop()
 tasks = []
-for i, item in enumerate(final):
-    if (i < 5000 or i > 7000):
+for item in final:
+    if (item['sku'] < 4500 or item['sku'] > 5500):
         continue
-    task = asyncio.ensure_future(make_parallel_post(item, i))
+    task = asyncio.ensure_future(make_parallel_post(item))
     tasks.append(task)
 loop.run_until_complete(asyncio.wait(tasks))
